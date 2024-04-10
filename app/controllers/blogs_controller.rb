36d500f37c9a3b4ebcd4fc77fb4fb4b2b1f23346
icchankun <ikkun.official@gmail.com>
2024-04-10 14:55:30 +0900

    他の人のブログを編集・削除できないように制限する。

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -4,6 +4,7 @@ class BlogsController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[index show]
 
   before_action :set_blog, only: %i[show edit update destroy]
+  before_action :ensure_current_user, only: %i[edit update destroy]
 
   def index
     @blogs = Blog.search(params[:term]).published.default_order
@@ -17,9 +18,7 @@ class BlogsController < ApplicationController
     @blog = Blog.new
   end
 
-  def edit
-    raise ActiveRecord::RecordNotFound if !@blog.owned_by?(current_user)
-  end
+  def edit; end
 
   def create
     @blog = current_user.blogs.new(blog_params)
@@ -49,6 +48,10 @@ class BlogsController < ApplicationController
 
   def set_blog
     @blog = Blog.find(params[:id])
+  end
+
+  def ensure_current_user
+    raise ActiveRecord::RecordNotFound if !@blog.owned_by?(current_user)
   end
 
   def blog_params
